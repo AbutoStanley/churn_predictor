1711ebd95dd6d4149525d3de479394c08864185d
Stanley <abutostanley@gmail.com>
2026-04-07 21:17:54 +0300

    cors change

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"
-                   ],
+                   "https://churn-predictor-2-19zc.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
